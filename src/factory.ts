108698f6615a35f8d82d11e61b6ad41aa573e300
Uche Chilaka <uche@lar.city>
2020-10-10 06:54:57 -0400

    config: added .workspace, turned off prettier and configured eslint for TS

--- src/factory.ts
+++ src/factory.ts
@@ -2,18 +2,18 @@ import cors from 'fastify-cors'
 import fastify, { FastifyInstance } from 'fastify'
 import hoistRoutes from './routes'
 
-const instance: FastifyInstance = fastify({ logger: true })
+const instance: FastifyInstance = fastify({ logger: true, })
 
 hoistRoutes(instance)
 
 instance.register(cors, {
-  origin: [/(\.)?larcity\.(com|dev|local)(:3030)?$/, /(\.)?lar\.city$/],
+  origin: [/(\.)?larcity\.(com|dev|local)(:3030)?$/, /(\.)?lar\.city$/,],
   // origin: (origin, cb) => {
   //   console.debug('cors::', { origin })
   //   cb(null, true)
   //   return
   // },
-  methods: ['GET', 'POST']
+  methods: ['GET', 'POST',],
 })
 
 export default instance

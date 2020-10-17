b9209e5b57443d97aabbda0e976cf165ce89f6bf
Uche Chilaka <uche@lar.city>
2020-10-17 12:37:00 -0400

    SAVEPOINT

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,6 @@ import factoryInstance, { setup } from './factory'
 
 const APP_PORT = Number(process.env.PORT ?? 3020)
 
-// factoryInstance.listen(APP_PORT, '0.0.0.0', (err, address) => {
-//   if (err) {
-//     factoryInstance.log.error(err)
-//     process.exit(1)
-//   }
-
-//   factoryInstance.log.info(`server listening on ${address}`)
-// })
-
 setup().then(factoryInstance => {
   factoryInstance.listen(APP_PORT, '0.0.0.0', (err, address) => {
     if (err) {
